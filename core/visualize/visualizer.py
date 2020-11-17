d78084df5131049ef53b9011f8525bd1318e3e8c
jianfenglihg <jianfenglihg@gmail.com>
2020-11-17 15:49:57 +0800

    meta

--- core/visualize/visualizer.py
+++ core/visualize/visualizer.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import PIL.Image as pil
 import matplotlib as mpl
 import matplotlib.cm as cm
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
 
 colorlib = [(0,0,255),(255,0,0),(0,255,0),(255,255,0),(0,255,255),(255,0,255),(0,0,0),(255,255,255)]
@@ -19,6 +20,45 @@ class Visualizer(object):
         self.dump_dir = dump_dir
 
         self.log_list = []
+        self.COLORMAPS = {'rainbow': self.opencv_rainbow(),
+                'magma': self.high_res_colormap(cm.get_cmap('magma')),
+                'bone': cm.get_cmap('bone', 10000)}
+
+    def high_res_colormap(self, low_res_cmap, resolution=1000, max_value=1):
+        # Construct the list colormap, with interpolated values for higer resolution
+        # For a linear segmented colormap, you can just specify the number of point in
+        # cm.get_cmap(name, lutsize) with the parameter lutsize
+        x = np.linspace(0, 1, low_res_cmap.N)
+        low_res = low_res_cmap(x)
+        new_x = np.linspace(0, max_value, resolution)
+        high_res = np.stack([np.interp(new_x, x, low_res[:, i]) for i in range(low_res.shape[1])], axis=1)
+        return ListedColormap(high_res)
+
+
+    def opencv_rainbow(self, resolution=1000):
+        # Construct the opencv equivalent of Rainbow
+        opencv_rainbow_data = (
+            (0.000, (1.00, 0.00, 0.00)),
+            (0.400, (1.00, 1.00, 0.00)),
+            (0.600, (0.00, 1.00, 0.00)),
+            (0.800, (0.00, 0.00, 1.00)),
+            (1.000, (0.60, 0.00, 1.00))
+        )
+        return LinearSegmentedColormap.from_list('opencv_rainbow', opencv_rainbow_data, resolution)
+
+    def tensor2array(self, tensor, max_value=None, colormap='rainbow'):
+        tensor = tensor.detach().cpu()
+        if max_value is None:
+            max_value = tensor.max().item()
+        if tensor.ndimension() == 2 or tensor.size(0) == 1:
+            norm_array = tensor.squeeze().numpy()/max_value
+            array = self.COLORMAPS[colormap](norm_array).astype(np.float32)
+            array = array.transpose(2, 0, 1)[:3]
+
+        elif tensor.ndimension() == 3:
+            assert(tensor.size(0) == 3)
+            array = 0.5 + tensor.numpy()*0.5
+        return array
 
     def add_log_pack(self, log_pack):
         self.log_list.append(log_pack)
